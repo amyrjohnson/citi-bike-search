2b5308eb64360b69b52a4d4d741bfdef64178abc
kylefdoherty <kyledoherty22@gmail.com>
2014-07-01 20:40:46 -0400

    added to dos

--- parse_feed.rb
+++ parse_feed.rb
@@ -15,60 +15,19 @@ class CBParser
     File.open("#{Time.now}.txt","w"){|file| file.write(broadway_station)}
   end 
 
-#collect data to file or db 
+### Creating website 
 #get website up with station data & saving to file or db
+#make git repo
+#create class to create instances of stations from data
+#type in name and resturn current state of that station 
+
+
+
+
+### Logging data giving access to it 
+#collect data to file or db 
+#research gathering and storing data and then accessing it 
 
 end 
 
 
-
-# response.keys
-# => ["executionTime", "stationBeanList"]
-
-# response.class
-# => HTTParty::Response - looks a lot like a hash 
-
-# response["stationBeanList"].count
-# => 332
-
-# response["stationBeanList"].select {|station| station["statusKey"] != 1}}
-# => returns array of 5 station hashes 
-
-
-
-
-
-
-# => [{"id"=>72,
- #  "stationName"=>"W 52 St & 11 Ave",
- #  "availableDocks"=>19,
- #  "totalDocks"=>38,
- #  "latitude"=>40.76727216,
- #  "longitude"=>-73.99392888,
- #  "statusValue"=>"In Service",
- #  "statusKey"=>1,
- #  "availableBikes"=>13,
- #  "stAddress1"=>"W 52 St & 11 Ave",
- #  "stAddress2"=>"",
- #  "city"=>"",
- #  "postalCode"=>"",
- #  "location"=>"",
- #  "altitude"=>"",
- #  "testStation"=>false,
- #  "lastCommunicationTime"=>nil,
- #  "landMark"=>""},
- # {"id"=>79,
- #  "stationName"=>"Franklin St & W Broadway",
- #  "availableDocks"=>32,
- #  "totalDocks"=>33,
- #  "latitude"=>40.71911552,
- #  "longitude"=>-74.00666661,
- #  "statusValue"=>"In Service",
- #  "statusKey"=>1,
- #  "availableBikes"=>1,
-
-
-
- #big_ass_docks = response["stationBeanList"].select {|station| station["totalDocks"] >= 50}
-
- # => An array of 32 station hashes 
